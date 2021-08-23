e3cd11b0247c79f7ad61d32c903623c09938dab9
goldenboyzwonder <goldenboyzwonder@gmail.com>
2021-08-23 04:26:53 -0500

    new version

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,6 @@
-print ('hello world, this is my first python code')
+#this logic uses a varible
+message = 'hello world, this is my first python code'
+print (message)
+
+#this logic prints the message
+print ('hello world!')
