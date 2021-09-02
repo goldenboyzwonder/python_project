38e93cbcebabb6347e83fdd28a35f163ee89f47b
goldenboyzwonder <goldenboyzwonder@gmail.com>
2021-09-02 01:03:00 -0500

    change

--- hello_world.py
+++ hello_world.py
@@ -4,3 +4,20 @@ print (message)
 
 #this logic prints the message
 print ('hello world!')
+
+#using variables
+firstText = "I love apples"
+print(firstText)
+
+#using variables that change
+firstText = "I love apples"
+print(firstText)
+firstText = "I love bananas"
+print(firstText)
+
+#String Methods
+string = "my name"
+print(string.capitalize())
+print(string.center(14))
+print(string.title())
+print(string.upper())
