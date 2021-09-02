5297ea7afed44a41a36e3a3e2d1417e5b14c74bf
goldenboyzwonder <goldenboyzwonder@gmail.com>
2021-09-02 01:32:17 -0500

    New examples

--- hello_world.py
+++ hello_world.py
@@ -21,3 +21,25 @@ print(string.capitalize())
 print(string.center(14))
 print(string.title())
 print(string.upper())
+
+#f-string or format string
+fName = "david"
+lName = "blaine"
+fulName = f"{fName} {lName}"
+print(fulName.title())
+
+animal1 = "bird"
+animal2 = "man"
+hero = f"{animal1} {animal2}"
+print(hero)
+
+fName = "Micheal"
+lName = "Jordan"
+fulName = f"{fName} {lName}"
+print(f"Hello, my name is {fulName.lower()}!")
+
+animal1 = "bat"
+#did not add the variable animal2 because the variable did not change and is already declared
+fulName = f"{animal1}{animal2}"
+message = f"The name is {fulName.upper()}"
+print(message)
